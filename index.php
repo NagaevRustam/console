700db2ac54579bdbe57dbb4d1e2590b17d495a42
NagaevRustam <nagaevrustam@yandex.ru>
2024-11-21 14:39:16 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $variable1 = 10;
+    $variable2 = 0;
+
+    if (!is_int($variable1)) {
+
+        echo 'Введите, пожалуйста, число';
+    };
+
+    if ($variable2 == 0) {
+        echo 'Делить на 0 нельзя';
+    };
+    //$variable3 = $variable1 / $variable2;
+    //echo $variable3;
+?>
